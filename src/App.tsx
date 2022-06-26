4485607bb8f0f07acae85ff985993c253d5bdd16
Jonathan Pichardo <jonathan.pichardo@classdojo.com>
2022-06-26 16:17:46 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import _ from "lodash";
+import React, { useContext } from "react";
+import "./App.css";
+
+import * as Game from "./Game";
+
+type GameContextType = {
+  highlightedCells: {
+    row: Game.Row;
+    col: Game.Col;
+  }[];
+  board: Game.Board;
+  movePiece: (row: Game.Row, col: Game.Col, piece: Game.Piece) => void;
+  highlightCells: (vertices: { row: Game.Row; col: Game.Col }[]) => void;
+};
+
+const GameContext = React.createContext<{
+  highlightedCells: {
+    row: Game.Row;
+    col: Game.Col;
+  }[];
+  highlightCells: (vertices: { row: Game.Row; col: Game.Col }[]) => void;
+}>({
+  highlightedCells: [],
+  highlightCells: () => {},
+});
+
+const Row = ({
+  alternateColors,
+  row,
+}: {
+  alternateColors?: boolean;
+  row: { [k in Game.Col]: Game.Vertice };
+}) => {
+  const cols = Object.keys(row) as Game.Col[];
+
+  const colorClasses = alternateColors
+    ? ["Black-Cell", "White-Cell"]
+    : ["White-Cell", "Black-Cell"];
+
+  return (
+    <div className="Row">
+      {cols.map((col, index) => (
+        <Cell
+          colorClass={colorClasses[index % 2]}
+          key={col}
+          vertice={row[col]}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Cell = ({
+  colorClass,
+  vertice,
+}: {
+  colorClass: string;
+  vertice: Game.Vertice;
+}) => {
+  const game = useContext(GameContext);
+  const highlightCells = () => {
+    const paths = Game.getPossiblePaths(vertice);
+
+    const cells = _.uniq(
+      paths
+        .flatMap(({ source, target }) => [source, target])
+        .filter(({ row, col }) => row !== vertice.row || col !== vertice.col)
+        .map((c) => ({ row: c.row, col: c.col }))
+    );
+
+    game.highlightCells(cells);
+  };
+
+  const isCellHighlighted = game.highlightedCells.some(
+    (c) => c.row === vertice.row && c.col === vertice.col
+  );
+
+  const className = [
+    colorClass,
+    isCellHighlighted ? "Highlighted-Cell" : "",
+  ].join(" ");
+
+  return (
+    <div className={className} onClick={highlightCells}>
+      {vertice.piece && <Piece piece={vertice.piece} />}
+    </div>
+  );
+};
+
+const Piece = ({ piece }: { piece: Game.Piece }) => {
+  return <div className="Piece">{piece.value}</div>;
+};
+
+const Board = ({ board }: { board: Game.Board }) => {
+  const rows = Object.keys(board) as Game.Row[];
+  const numberOfRows = rows.length;
+
+  const [highlightedCells, setHighlightedCells] = React.useState<
+    {
+      row: Game.Row;
+      col: Game.Col;
+    }[]
+  >([]);
+
+  return (
+    <GameContext.Provider
+      value={{ highlightedCells, highlightCells: setHighlightedCells }}
+    >
+      <div className="Board">
+        {rows.map((row, index) => (
+          <>
+            <Row key={row} alternateColors={index % 2 === 0} row={board[row]} />
+            {index + 1 === numberOfRows / 2 && (
+              <div className="Divider" key={"divider"}></div>
+            )}
+          </>
+        ))}
+      </div>
+    </GameContext.Provider>
+  );
+};
 
 function App() {
+  const board = Game.generateBoard();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <Board board={board} />
+      </div>
     </div>
   );
 }
---
+++ src/Game.ts
@@ -0,0 +1,281 @@
+import _ from "lodash";
+
+const COLS = ["a", "b", "c", "d"] as const;
+const ROWS = ["1", "2", "3", "4", "5", "6", "7", "8"] as const;
+
+export type Row = typeof ROWS[number];
+export type Col = typeof COLS[number];
+
+export type Direction = "h" | "v" | "d";
+
+export type Vertice = {
+  row: Row;
+  col: Col;
+  edges: Edge[];
+  piece: Piece;
+};
+
+export type Edge = {
+  source: Vertice;
+  target: Vertice;
+  direction: Direction;
+};
+
+export type Board = { [key in Row]: { [key in Col]: Vertice } };
+
+export type Piece = {
+  row: Row;
+  col: Col;
+  value: number;
+  allowedMovements: Direction[];
+  movementLength: number;
+};
+
+export const generateBoard = () => {
+  const board = addEdges(getAllVertices());
+
+  const initialPieces = getInitialPieces();
+
+  initialPieces.forEach((piece) => (board[piece.row][piece.col].piece = piece));
+
+  return board;
+};
+
+export const getAllVertices = () => {
+  const board: any = {};
+
+  ROWS.forEach((row) => {
+    board[row] = {};
+    COLS.forEach(
+      (col) => (board[row][col] = { row: row, col: col, edges: [] })
+    );
+  });
+
+  return board;
+};
+
+export const addEdges = (board: any) => {
+  ROWS.forEach((row) => {
+    COLS.forEach((col) => {
+      const vertice = board[row][col];
+      const edges = getPossibleEdges(vertice, board);
+      board[row][col].edges = edges;
+    });
+  });
+
+  return board;
+};
+
+export const getPossibleEdges = (vertice: Vertice, board: any) => {
+  const indexOfRow = ROWS.indexOf(vertice.row);
+  const indexOfCol = COLS.indexOf(vertice.col);
+
+  const possibleRows = _.compact([
+    ROWS[indexOfRow - 1],
+    ROWS[indexOfRow + 1],
+    vertice.row,
+  ]);
+
+  const possibleCols = _.compact([
+    COLS[indexOfCol - 1],
+    COLS[indexOfCol + 1],
+    vertice.col,
+  ]);
+
+  const possibleEdges = possibleRows.reduce((acc, row) => {
+    possibleCols.forEach((col) => {
+      if (row === vertice.row && col === vertice.col) return acc;
+
+      acc.push({
+        source: vertice,
+        target: board[row][col],
+        direction: getEdgeDirection(row, col, vertice),
+      });
+    });
+    return acc;
+  }, [] as Edge[]);
+
+  return possibleEdges;
+};
+
+export const getEdgeDirection = (
+  edgeRow: Row,
+  edgeCol: Col,
+  vertice: Vertice
+) => {
+  const indexOfRow = ROWS.indexOf(vertice.row);
+  const indexOfEdgeRow = ROWS.indexOf(edgeRow);
+  const rowDirection = indexOfEdgeRow - indexOfRow;
+
+  if (rowDirection === 0) return "h";
+
+  const indexOfCol = COLS.indexOf(vertice.col);
+  const indexOfEdgeCol = COLS.indexOf(edgeCol);
+  const colDirection = indexOfEdgeCol - indexOfCol;
+
+  if (colDirection === 0) return "v";
+
+  return "d";
+};
+
+export const getPossiblePaths = (vertice: Vertice): Edge[] => {
+  const paths = vertice.edges
+    .filter((edge) => vertice.piece.allowedMovements.includes(edge.direction))
+    .filter((edge) => edge.target.piece === undefined);
+
+  const nextPaths = paths.flatMap((path) => {
+    const xDifference =
+      COLS.indexOf(path.target.col) - COLS.indexOf(vertice.col);
+
+    const yDifference =
+      ROWS.indexOf(path.target.row) - ROWS.indexOf(vertice.row);
+
+    return getPossiblePathsWithDifferences(
+      path.target,
+      vertice.piece.allowedMovements,
+      vertice.piece.movementLength - 1,
+      xDifference,
+      yDifference
+    );
+  });
+
+  return [...paths, ...nextPaths];
+};
+
+export const getPossiblePathsWithDifferences = (
+  vertice: Vertice,
+  allowedDirections: Direction[],
+  length: number,
+  xDiff: number,
+  yDiff: number
+): Edge[] => {
+  if (!length) return [];
+
+  const paths = vertice.edges.filter((edge) => {
+    if (!allowedDirections.includes(edge.direction)) return false;
+
+    const xDifference =
+      COLS.indexOf(edge.target.col) - COLS.indexOf(vertice.col);
+
+    const yDifference =
+      ROWS.indexOf(edge.target.row) - ROWS.indexOf(vertice.row);
+
+    if (xDiff !== xDifference || yDiff !== yDifference) return false;
+
+    return true;
+  }, [] as Edge[]);
+
+  const nextPaths = paths.flatMap((path) =>
+    getPossiblePathsWithDifferences(
+      path.target,
+      [path.direction],
+      length - 1,
+      xDiff,
+      yDiff
+    )
+  );
+
+  return [...paths, ...nextPaths];
+};
+
+export const getPathCoordinates = (edge: Edge) => {
+  return `${edge.source.row}, ${edge.source.col} - ${edge.target.row}, ${edge.target.col} -> ${edge.direction}`;
+};
+
+export const getInitialPieces = () => {
+  return [...getUpsidePieces(), ...getDownsidePieces()];
+};
+
+const getDownsidePieces = () => {
+  const startingRow: Row = ROWS[ROWS.length - 1];
+  const startingCol: Col = COLS[COLS.length - 1];
+
+  const pieces: Piece[] = [];
+
+  const rowIdx = ROWS.indexOf(startingRow);
+  const possibleRows = [startingRow, ROWS[rowIdx - 1], ROWS[rowIdx - 2]];
+
+  const colIdk = COLS.indexOf(startingCol);
+  const possibleCols = [startingCol, COLS[colIdk - 1], COLS[colIdk - 2]];
+
+  possibleRows.forEach((row) => {
+    const rowDiff = ROWS.indexOf(startingRow) - ROWS.indexOf(row);
+
+    possibleCols.forEach((col) => {
+      const colDiff = COLS.indexOf(startingCol) - COLS.indexOf(col);
+
+      const totalDiff = rowDiff + colDiff;
+
+      const value = getValueFromDiff(totalDiff);
+      const allowedMovements = getAllowedMovesFromValue(value);
+      const movementLength = getMovementLengthFromValue(value);
+
+      pieces.push({
+        row,
+        col,
+        value,
+        allowedMovements,
+        movementLength,
+      });
+    });
+  });
+
+  return pieces;
+};
+
+const getUpsidePieces = () => {
+  const startingRow: Row = ROWS[0];
+  const startingCol: Col = COLS[0];
+
+  const pieces: Piece[] = [];
+
+  const rowIdx = ROWS.indexOf(startingRow);
+  const possibleRows = [startingRow, ROWS[rowIdx + 1], ROWS[rowIdx + 2]];
+
+  const colIdk = COLS.indexOf(startingCol);
+  const possibleCols = [startingCol, COLS[colIdk + 1], COLS[colIdk + 2]];
+
+  possibleRows.forEach((row) => {
+    const rowDiff = ROWS.indexOf(row) - ROWS.indexOf(startingRow);
+
+    possibleCols.forEach((col) => {
+      const colDiff = COLS.indexOf(col) - COLS.indexOf(startingCol);
+
+      const totalDiff = rowDiff + colDiff;
+
+      const value = getValueFromDiff(totalDiff);
+      const allowedMovements = getAllowedMovesFromValue(value);
+      const movementLength = getMovementLengthFromValue(value);
+
+      pieces.push({
+        row,
+        col,
+        value,
+        allowedMovements,
+        movementLength,
+      });
+    });
+  });
+
+  return pieces;
+};
+
+const getValueFromDiff = (diff: number) => {
+  if (diff === 1) return 3;
+  if (diff === 2) return 2;
+  if (diff >= 3) return 1;
+  return 3;
+};
+
+const getAllowedMovesFromValue = (value: number): Direction[] => {
+  if (value === 3) return ["h", "v", "d"];
+  if (value === 2) return ["h", "v"];
+  if (value === 1) return ["d"];
+  return [];
+};
+
+const getMovementLengthFromValue = (value: number) => {
+  if (value === 2) return 2;
+  if (value === 1) return 1;
+  return 7; // Just the biggest number possible
+};
